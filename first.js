baf7e51126ec33fb128c18eea03d4129bb8d1b08
Jananiviswa15 <73423520+Jananiviswa15@users.noreply.github.com>
2023-08-21 18:40:14 +0530

    sample js files

---
+++ first.js
@@ -0,0 +1,89 @@
+let name = "name value";
+let name1 = 'name1 value';
+
+console.log(name);
+console.log(typeof name);
+name1 = 90;
+console.log(name1);
+console.log(typeof name1);
+name1 = 90.78;  //decimal 
+console.log(name1);
+console.log(typeof name1);
+
+
+const piValue = 3.14
+let circleRadius = 9
+
+area = circleRadius*circleRadius*piValue;
+console.log(area);
+
+
+let address = "streetname\tcityname\tpin\ncode";
+console.log(address);
+
+
+let age
+console.log(age)
+
+
+
+age = 78;
+console.log(typeof ("hello" / "hi"+90));
+
+console.log(age / "hi"+90);
+
+
+let numberVal = Number("string");
+console.log("===="+numberVal);
+
+
+let numberVal1 = Number("string5457457");
+console.log("===="+numberVal1);
+
+
+
+let numberVal2 = Number("96868");
+console.log("===="+numberVal2);
+console.log(typeof numberVal2)
+
+
+let num ;
+console.log(typeof num);
+
+num = 10;
+console.log(typeof num);
+
+num = num +"";
+console.log(typeof num);
+
+num = num - 2;
+console.log(typeof num);
+console.log( !num);
+
+console.log(Boolean(num))
+
+
+
+
+
+
+
+let result = Math.pow(num, 3);  //num ** 3
+console.log(result)
+
+
+let val = "8" < 90;
+console.log(val)
+
+
+
+let data = '';
+let val1 = false;
+console.log(data === val1)
+
+
+let data1 = 10, data2 = 90, data3 = 90
+
+console.log(data1 < data2 < data3)
+
+
---
+++ second.js
@@ -0,0 +1,51 @@
+let val1 = 10;
+let val2 = 20;
+
+
+let result = val1 < val2;
+
+
+if(result)
+	console.log("hi")
+
+else{
+	
+	
+}
+
+
+result = val1%2 === 0 ? "even " : "odd"
+console.log(result)
+
+
+
+let color = "blue"
+
+
+switch(color){
+	
+	
+	case "monday":
+	
+	case "blue":
+	        console.log("hello")
+	
+	
+	
+}
+
+
+
+console.log(`hello all ${color} and value is ${val1}`);
+
+let person = {
+	
+	name : "janani",
+	age : 28
+	
+}
+
+
+
+console.log(person.age);
+console.log(person['age'])
